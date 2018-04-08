935423549b7e9e985a7c4ed81db1281ef0637dec
owner <t>
2018-04-08 16:41:34 +0900

    コミット

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,13 +1,13 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use \DB;
+use DB;
 
 class UserTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
+     * 
      * @return void
      */
     public function run()
